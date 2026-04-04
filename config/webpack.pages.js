cfe0d2c0ec9a7451643a923d9ad35ac22e56704a
Тимур <timmy.rio@yandex.ru>
2026-04-04 10:49:23 +0300

    update webpack pages

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -11,10 +11,8 @@ function createPages(template, filename, chunks) {
 const htmlPages = [
   createPages('./src/index.html', './index.html', ['index']),
   createPages('./src/pages/articles.html', './pages/articles.html', ['index']),
-  createPages('./src/pages/tests.html', './pages/tests.html', ['index']),
-  createPages('./src/pages/dictionary.html', './pages/dictionary.html', [
-    'index'
-  ]),
+
+
   createPages(
     './src/pages/articles/first_art.html',
     './pages/articles/first_art.html',
@@ -30,9 +28,29 @@ const htmlPages = [
     './pages/articles/third_art.html',
     ['index']
   ),
+
+
+  createPages('./src/pages/tests.html', './pages/tests.html', ['index']),
+  createPages('./src/pages/checklists.html', './pages/checklists.html', [
+    'index'
+  ]),
+  createPages('./src/pages/resources.html', './pages/resources.html', [
+    'index'
+  ]),
+  createPages('./src/pages/about.html', './pages/about.html', [
+    'index'
+  ]),
+  createPages('./src/pages/styleguide.html', './pages/styleguide.html', [
+    'index'
+  ]),
+  createPages('./src/pages/sitemap.html', './pages/sitemap.html', [
+    'index'
+  ]),
+  
   createPages('./src/pages/tests/test1.html', './pages/tests/test1.html', [
     'index'
   ])
 ]
 
 module.exports = htmlPages
+
